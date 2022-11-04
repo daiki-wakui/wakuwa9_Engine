f6137fcc844869a1df8c25d08041aa4fecaadb16
DaikiWakui <k021g1119@g.neec.ac.jp>
2022-11-04 16:11:48 +0900

    エラー修正

--- KeyBoard.cpp
+++ KeyBoard.cpp
@@ -10,20 +10,20 @@ void KeyBoard::Update()
 
 bool KeyBoard::keyPush(uint8_t key)
 {
-	
+	return false;
 }
 
 bool KeyBoard::keyRelease(uint8_t key)
 {
-	
+	return false;
 }
 
 bool KeyBoard::keyInstantPush(uint8_t key)
 {
-	
+	return false;
 }
 
 bool KeyBoard::keyInstantRelease(uint8_t key)
 {
-	
+	return false;
 }
