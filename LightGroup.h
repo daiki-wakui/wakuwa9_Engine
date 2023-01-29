6bdd7dd017fb47f440e11af2d8ca059e0b8f39d8
DaikiWakui <k021g1119@g.neec.ac.jp>
2023-01-30 03:10:36 +0900

    soptLight途中

--- LightGroup.h
+++ LightGroup.h
@@ -8,6 +8,7 @@
 
 #include "DirectionalLight.h"
 #include "PointLight.h"
+#include "SpotLight.h"
 
 //ライト
 class LightGroup
@@ -27,6 +28,7 @@ private:// エイリアス
 public: //定数
 	static const int DirLightNum = 3;
 	static const int PointLightNum = 3;
+	static const int SpotLightNum = 3;
 
 public: //サブクラス
 
@@ -40,6 +42,8 @@ public: //サブクラス
 		DirectionalLight::ConstBufferData dirLights[DirLightNum];
 		//点光源用
 		PointLight::ConstBufferData pointLights[PointLightNum];
+		//スポットライト用
+		SpotLight::ConstBufferData spotLights[SpotLightNum];
 	};
 
 public: // 静的メンバ関数
@@ -97,6 +101,20 @@ public: //メンバ関数
 
 	void SetPointLightAtten(int index, const XMFLOAT3& lightatten);
 
+
+	//スポットライト
+	void SetSpotLightActive(int index, bool active);
+
+	void SetSpotLightDir(int index, const XMVECTOR& lightdir);
+
+	void SetSpotLightPos(int index, const XMFLOAT3& lightpos);
+
+	void SetSpotLightColor(int index, const XMFLOAT3& lightcolor);
+
+	void SetSpotLightAtten(int index, const XMFLOAT3& lightAtten);
+
+	void SetSpotLightFactorAngle(int index, XMFLOAT2& lightFactorAngle);
+
 private: //メンバ変数
 
 	//定数バッファ
@@ -110,6 +128,9 @@ private: //メンバ変数
 
 	//点光源の配列
 	PointLight pointLights[PointLightNum];
+
+	//スポットライトの配列
+	SpotLight spotLights[SpotLightNum];
 
 	//ダーティフラグ
 	bool dirty = false;
