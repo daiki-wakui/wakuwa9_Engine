6bfc9c814867d15c41776dd8eb7f032ec0605e9c
DaikiWakui <k021g1119@g.neec.ac.jp>
2023-05-24 15:25:09 +0900

    再帰処理

--- LevelLoader.cpp
+++ LevelLoader.cpp
@@ -62,11 +62,11 @@ LevelData* LevelLoader::LoadFile(const std::string& fileName)
 			// 平行移動
 			objectData.translation.m128_f32[0] = (float)transform["translation"][1];
 			objectData.translation.m128_f32[1] = (float)transform["translation"][2];
-			objectData.translation.m128_f32[2] = (float)-transform["translation"][0];
+			objectData.translation.m128_f32[2] = -(float)transform["translation"][0];
 			objectData.translation.m128_f32[3] = 1.0f;
 			// 回転角
-			objectData.rotation.m128_f32[0] = (float)-transform["rotation"][1];
-			objectData.rotation.m128_f32[1] = (float)-transform["rotation"][2];
+			objectData.rotation.m128_f32[0] = -(float)transform["rotation"][1];
+			objectData.rotation.m128_f32[1] = -(float)transform["rotation"][2];
 			objectData.rotation.m128_f32[2] = (float)transform["rotation"][0];
 			objectData.rotation.m128_f32[3] = 0.0f;
 			// スケーリング
@@ -77,9 +77,48 @@ LevelData* LevelLoader::LoadFile(const std::string& fileName)
 		}
 
 		if (object.contains("children")) {
+			// "objects"の全オブジェクトを走査
+			for (nlohmann::json& object : deserialized["objects"]) {
+				assert(object.contains("type"));
 
+				// 種別を取得
+				std::string type = object["type"].get<std::string>();
+
+				// MESH
+				if (type.compare("MESH") == 0) {
+					// 要素追加
+					levelData->objects.emplace_back(LevelData::ObjectData{});
+					// 今追加した要素の参照を得る
+					LevelData::ObjectData& objectData = levelData->objects.back();
+
+					if (object.contains("file_name")) {
+						// ファイル名
+						objectData.fileName = object["file_name"];
+					}
+
+					// トランスフォームのパラメータ読み込み
+					nlohmann::json& transform = object["transform"];
+					// 平行移動
+					objectData.translation.m128_f32[0] = (float)transform["translation"][1];
+					objectData.translation.m128_f32[1] = (float)transform["translation"][2];
+					objectData.translation.m128_f32[2] = -(float)transform["translation"][0];
+					objectData.translation.m128_f32[3] = 1.0f;
+					// 回転角
+					objectData.rotation.m128_f32[0] = -(float)transform["rotation"][1];
+					objectData.rotation.m128_f32[1] = -(float)transform["rotation"][2];
+					objectData.rotation.m128_f32[2] = (float)transform["rotation"][0];
+					objectData.rotation.m128_f32[3] = 0.0f;
+					// スケーリング
+					objectData.scaling.m128_f32[0] = (float)transform["scaling"][1];
+					objectData.scaling.m128_f32[1] = (float)transform["scaling"][2];
+					objectData.scaling.m128_f32[2] = (float)transform["scaling"][0];
+					objectData.scaling.m128_f32[3] = 0.0f;
+				}
+			}
 		}
 	}
 
     return levelData;
 }
+
+
