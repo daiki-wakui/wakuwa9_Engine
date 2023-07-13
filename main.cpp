1cbcd63b68669cbda72934be28a8517d5aa17bd1
daiki-wakui <k021g1119@g.neec.ac.jp>
2023-07-13 11:14:43 +0900

    プッシュし忘れ

--- main.cpp
+++ main.cpp
@@ -4,28 +4,10 @@
 
 int32_t WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int32_t) {
 
-	//GameCore game;
-
 	std::unique_ptr<Framework> game_ = std::make_unique<GameCore>();
 
-	/*game.Initialize();
-	MSG msg{};*/
-
-	game_->Run();
-
 	//ゲームループ
-	//while (true) {
-	//	//×ボタンで終了メッセージがきたら
-	//	if (game.GetIsEnd()) {
-	//		break;	//ゲームループ終了
-	//	}
-
-	//	game.Update();
-
-	//	game.Draw();
-	//}
-
-	//game.Finalize();
+	game_->Run();
 
 	return 0;
 }
