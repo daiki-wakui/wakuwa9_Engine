05a274e56a18def321af8685ca0f1f6800032708
daiki-wakui <k021g1119@g.neec.ac.jp>
2023-10-10 15:57:56 +0900

    基本的な関数追加

--- LevelEditor.cpp
+++ LevelEditor.cpp
@@ -1,1 +1,17 @@
 #include "LevelEditor.h"
+
+void LevelEditor::Initialize()
+{
+}
+
+void LevelEditor::Update()
+{
+}
+
+void LevelEditor::Darw()
+{
+}
+
+void LevelEditor::ReLoad()
+{
+}
--- LevelEditor.h
+++ LevelEditor.h
@@ -1,5 +1,21 @@
 #pragma once
+#include "Model.h"
+#include "EventBox.h"
+#include "Object3D.h"
+#include "LevelLoader.h"
+
 class LevelEditor
 {
+private:
+
+	LevelData leveldata_;
+
+public:
+
+	void Initialize();
+	void Update();
+	void Darw();
+	void ReLoad();
+
 };
 
